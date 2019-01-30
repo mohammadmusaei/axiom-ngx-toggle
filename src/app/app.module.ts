a10a430af063414cbf40b7dc0787890a60587900
Mohammad <mohammad.musaei@gmail.com>
2019-01-30 10:15:55 +0330

    add toggle component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AxiomToggleDirective } from './axiom-toggle/axiom-toggle.directive';
 import { AxiomToggleComponent } from './axiom-toggle/axiom-toggle/axiom-toggle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AxiomToggleDirective,
     AxiomToggleComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
     AppRoutingModule
   ],
   providers: [],
--- src/app/axiom-toggle/axiom-toggle/axiom-toggle.component.ts
+++ src/app/axiom-toggle/axiom-toggle/axiom-toggle.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-axiom-toggle',
+  selector: '[ax-toggle]',
   templateUrl: './axiom-toggle.component.html',
-  styleUrls: ['./axiom-toggle.component.scss']
+  styleUrls: ['./axiom-toggle.component.scss'],
+  encapsulation : ViewEncapsulation.None
 })
 export class AxiomToggleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _renderer : Renderer2,private _element : ElementRef) { }
 
   ngOnInit() {
+    var parent = this._element.nativeElement.parentElement;
+    this._renderer.addClass(this._element.nativeElement, "checkbox");
+    //create wrapper
+    var wrapper = this._renderer.createElement("div");
+    this._renderer.addClass(wrapper, "button");
+    this._renderer.addClass(wrapper, "r");
+    this._renderer.addClass(wrapper, "checkbox-button");
+    this._renderer.appendChild(wrapper, this._element.nativeElement);
+    //add siblings
+    var ks = this._renderer.createElement("div");
+    this._renderer.addClass(ks, "ax-toggle-button");
+    var layer = this._renderer.createElement("div");
+    this._renderer.addClass(layer, "ax-toggle-layer");
+    this._renderer.appendChild(wrapper, ks);
+    this._renderer.appendChild(wrapper, layer);
+
+    this._renderer.insertBefore(parent, wrapper, parent.firstElementChild);
   }
 
 }
